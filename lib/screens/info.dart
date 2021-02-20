3d95325af3505a425cd034033f590acf2dbaa225
gdeus <dedeus.gabriel@gmail.com>
2021-02-20 16:57:03 -0300

    decreto

--- lib/screens/info.dart
+++ lib/screens/info.dart
@@ -7,7 +7,7 @@ class Info extends StatelessWidget {
     return Column(
       children: <Widget>[
         Text("Informações do aplicativo", style: GoogleFonts.lato(fontSize: 24),),
-        Text("Aplicativo criado baseado no Diário Oficial do Município de São Miguel do Iguaçu (Edição nº 2235 | 20/01/2020) onde é definido os plantões das farmácias em 2020. Caso o aplicativo esteja com problema ou informando a farmácia incorrreta "
+        Text("Aplicativo criado baseado no Diário Oficial do Município de São Miguel do Iguaçu (Edição nº 2525 | 12/02/2021) onde é definido os plantões das farmácias em 2021. Caso o aplicativo esteja com problema ou informando a farmácia incorrreta "
             "favor verificar as configurações de data e hora do seu celular. Dúvidas, informações, problemas ou bugs, favor entrar no contato no e-mail dedeus.gabriel@gmail.com", style: GoogleFonts.lato(fontSize: 20), textAlign: TextAlign.center, ),
       ],
     );
